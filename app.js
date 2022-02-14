d554344475d210fcb4df6ca45c4f1be2fcc496de
Lucas Henrique <lucashgp.l@hotmail.com>
2022-02-14 19:07:31 -0300

    localStorage carrinho

--- app.js
+++ app.js
@@ -59,8 +59,20 @@ const vm = new Vue({
     removerItem(index) {
       this.carrinho.splice(index, 1);
     },
+
+    checarLocalStorage() {
+      if (window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
+    },
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    },
   },
   created() {
     this.fetchProdudos();
+    this.checarLocalStorage();
   },
 });
