fba626eec8b6293e8848bbae06187652648f7f6f
Lucas Henrique <lucashgp.l@hotmail.com>
2022-02-14 19:48:23 -0300

    router

--- app.js
+++ app.js
@@ -77,8 +77,19 @@ const vm = new Vue({
         this.alertaAtivo = false;
       }, 1500);
     },
+    router() {
+      const hash = document.location.hash;
+      if (hash) {
+        this.fetchProdudo(hash.replace("#", ""));
+      }
+    },
   },
   watch: {
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+    },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
     },
@@ -86,5 +97,6 @@ const vm = new Vue({
   created() {
     this.fetchProdudos();
     this.checarLocalStorage();
+    this.router();
   },
 });
