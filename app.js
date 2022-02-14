211da9a19c7076c633be838f0bfee57be0052257
Lucas Henrique <lucashgp.l@hotmail.com>
2022-02-14 15:25:47 -0300

    lista-produtos

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const vm = new Vue({
-    el: "#app",
-    data: {
-        produtos: []
-    }
-    , methods: {
-        fetchProdudos() {
-            fetch("./api/produtos.json")
-                .then(r => r.json())
-                .then(r => {
-                    this.produtos = r
-                })
-        }
+  el: "#app",
+  data: {
+    produtos: [],
+  },
+  methods: {
+    fetchProdudos() {
+      fetch("./api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
     },
-    created() {
-        this.fetchProdudos();
-    }
-})
+  },
+  created() {
+    this.fetchProdudos();
+  },
+});
