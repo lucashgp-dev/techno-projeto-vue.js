00c1bfcfb7e6853ba91f6971995f967f555a2ec3
Lucas Henrique <lucashgp.l@hotmail.com>
2022-02-15 07:19:23 -0300

    estilo carrinho

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const vm = new Vue({
     carrinho: [],
     mensagemAlerta: "Item adicionado",
     alertaAtivo: false,
+    carrinhoAtivo: false,
   },
 
   filters: {
@@ -52,6 +53,9 @@ const vm = new Vue({
     },
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
+    },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
     },
     adicionarItem() {
       this.produto.estoque--;
